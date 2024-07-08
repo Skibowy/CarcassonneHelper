ed392dd621d3b26256c33e47b787ccd428e291ad
Skibowy <dawiskibabartosz@gmail.com>
2024-07-08 23:53:33 +0200

    Home page

--- src/main/java/com/project/CarcassonneHelper/HelloGui.java
+++ src/main/java/com/project/CarcassonneHelper/HelloGui.java
@@ -1,9 +1,35 @@
 package com.project.CarcassonneHelper;
 
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route()
-public class HelloGui {
+@Route(value = "")
+@PageTitle("Home")
+public class HelloGui extends VerticalLayout {
 
-    
+    public HelloGui() {
+        var todosList = new VerticalLayout();
+        var taskField = new TextField();
+        var addButton = new Button("Add");
+        addButton.addClickListener(click -> {
+            Checkbox checkbox = new Checkbox(taskField.getValue());
+            todosList.add(checkbox);
+        });
+        addButton.addClickShortcut(Key.ENTER);
+        add(
+                new H1("Vaadin Todo"),
+                todosList,
+                new HorizontalLayout(
+                        taskField,
+                        addButton
+                )
+        );
+    }
 }
