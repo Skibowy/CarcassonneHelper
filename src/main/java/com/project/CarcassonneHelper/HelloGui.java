68ee5e3005ae503317fe278b01351dd67be808ff
Skibowy <dawiskibabartosz@gmail.com>
2024-07-08 23:34:34 +0200

    First commit

---
+++ src/main/java/com/project/CarcassonneHelper/HelloGui.java
@@ -0,0 +1,9 @@
+package com.project.CarcassonneHelper;
+
+import com.vaadin.flow.router.Route;
+
+@Route()
+public class HelloGui {
+
+    
+}
---
+++ src/test/java/com/project/CarcassonneHelper/CarcassonneHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CarcassonneHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcassonneHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
